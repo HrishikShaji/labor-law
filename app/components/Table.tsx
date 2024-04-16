84a28df5ebe0276cf5e777b8851c4e444e3ff08e
HrishikShaji <hrishikminishaji@gmail.com>
2024-04-16 22:09:18 +0530

    added table

---
+++ app/components/Table.tsx
@@ -0,0 +1,183 @@
+export const Table = () => {
+	return (
+		<div
+			className="w-full h-full flex flex-col justify-center items-center p-10"
+			dir="ltr"
+		>
+			<table className="waffle ">
+				<thead>
+					<tr>
+						<th className="row-header freezebar-origin-ltr"></th>
+						<th id="1060074110C0" className="column-headers-background"></th>
+						<th id="1060074110C1" className="column-headers-background"></th>
+						<th id="1060074110C2" className="column-headers-background"></th>
+						<th id="1060074110C3" className="column-headers-background"></th>
+						<th id="1060074110C4" className="column-headers-background"></th>
+						<th id="1060074110C5" className="column-headers-background"></th>
+						<th id="1060074110C6" className="column-headers-background"></th>
+						<th id="1060074110C7" className="column-headers-background"></th>
+						<th id="1060074110C8" className="column-headers-background"></th>
+						<th id="1060074110C9" className="column-headers-background"></th>
+						<th id="1060074110C10" className="column-headers-background"></th>
+						<th id="1060074110C11" className="column-headers-background"></th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<th id="1060074110R0" className="row-headers-background"></th>
+						<td className="s0">
+							FORM I<br />
+							[Rule 22 (4)]
+							<br />
+							REGISTER OF FINES
+						</td>
+					</tr>
+					<tr>
+						<th id="1060074110R1" className="row-headers-background"></th>
+						<td className="s1">Name of the Employer</td>
+						<td className="s2"></td>
+					</tr>
+					<tr>
+						<th id="1060074110R2" className="row-headers-background"></th>
+						<td className="s3">Sr. No.</td>
+						<td className="s3">Empcd</td>
+						<td className="s3">Name</td>
+						<td className="s3">Father&#39;s / Husband&#39;s name</td>
+						<td className="s3">Sex</td>
+						<td className="s3">Department</td>
+						<td className="s3">
+							Nature and date of offence for which fine imposed
+						</td>
+						<td className="s3">
+							Whether worker showed cause against fine or not. if so, enter date
+						</td>
+						<td className="s3">Rates of wages</td>
+						<td className="s3">Date and amount of fine imposed</td>
+						<td className="s3">Date on which fine realised</td>
+						<td className="s3">Remarks</td>
+					</tr>
+					<tr>
+						<th id="1060074110R3" className="row-headers-background"></th>
+						<td className="s4">1</td>
+						<td className="s4"></td>
+						<td className="s4">2</td>
+						<td className="s4">3</td>
+						<td className="s4">4</td>
+						<td className="s4">5</td>
+						<td className="s4">6</td>
+						<td className="s4">7</td>
+						<td className="s4">8</td>
+						<td className="s4">9</td>
+						<td className="s4">10</td>
+						<td className="s4">11</td>
+					</tr>
+					<tr>
+						<th id="1060074110R4" className="row-headers-background"></th>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+					</tr>
+					<tr>
+						<th id="1060074110R5" className="row-headers-background"></th>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+					</tr>
+					<tr>
+						<th id="1060074110R6" className="row-headers-background"></th>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+					</tr>
+					<tr>
+						<th id="1060074110R7" className="row-headers-background"></th>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+					</tr>
+					<tr>
+						<th id="1060074110R8" className="row-headers-background"></th>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+					</tr>
+					<tr>
+						<th id="1060074110R9" className="row-headers-background"></th>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+					</tr>
+					<tr>
+						<th id="1060074110R10" className="row-headers-background"></th>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+						<td className="s5"></td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	);
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import { Dashboard } from "./components/Dashboard";
 import { SideBar } from "./components/SideBar";
+import { Table } from "./components/Table";
 
 export default function Home() {
   return (
     <main className="flex">
-      <SideBar />
-      <Dashboard />
+      <Table />
     </main>
   );
 }
