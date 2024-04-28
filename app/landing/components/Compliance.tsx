47e5a23bb2ca9db610303853bd1dd3fb730581ce
HrishikShaji <hrishikminishaji@gmail.com>
2024-04-29 04:35:57 +0530

    added compliance

---
+++ app/landing/components/Compliance.tsx
@@ -0,0 +1,36 @@
+export const Compliance = () => {
+  return (
+    <div className="px-5">
+      <div className="w-full rounded-l-2xl bg-[#0B091C] text-white p-5 pl-10 flex flex-col">
+        <div className="flex gap-5  py-5 items-center border-b-[1px] border-white">
+          <div className="flex flex-col gap-5 ">
+            <h1 className="text-[60px] leading-none font-bold w-[80%]">
+              Easy Compliance Management
+            </h1>
+            <p className="leading-loose w-[80%]">{`Our software can do the transformation of compliance management processes to save from potential financial and regulatoryrisks. Our software will helpyou to identify, assess, and mitigate potential risks and in the process alignyou with the industry regulations and standards.`}</p>
+            <div className="flex gap-5">
+              <button className="px-5 py-2 rounded-3xl border-white border-[1px]">
+                Sign In
+              </button>
+              <button className="px-5 py-2 rounded-3xl bg-[#D64100]">
+                Register
+              </button>
+            </div>
+          </div>
+          <div className="flex-shrink-0 h-[400px] w-[300px] rounded-xl bg-[#D64100]"></div>
+        </div>
+        <div className="flex w-full items-center justify-between py-4">
+          <div className="flex flex-col gap-2">
+            <h1>support@legumai.com</h1>
+            <h1>support@legumai.com</h1>
+          </div>
+          <div className="flex gap-6">
+            <div className="h-10 w-10 rounded-md bg-white"></div>
+            <div className="h-10 w-10 rounded-md bg-white"></div>
+            <div className="h-10 w-10 rounded-md bg-white"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- app/landing/page.tsx
+++ app/landing/page.tsx
@@ -1,4 +1,5 @@
 import { AwardsAndRecognitions } from "./components/AwardsAndRecognitions";
+import { Compliance } from "./components/Compliance";
 import { Features } from "./components/Features";
 import { Hero } from "./components/Hero";
 import { Intro } from "./components/Intro";
@@ -6,17 +7,17 @@ import { Navbar } from "./components/Navbar";
 import { Updates } from "./components/Updates";
 
 const Page = () => {
-	return (
-		<div className="w-full  h-full flex flex-col">
-			<Navbar />
-			<Hero />
-			<Intro />
-			<AwardsAndRecognitions />
-			<Features />
-			<Updates />
-
-		</div>
-	);
+  return (
+    <div className="w-full  h-full flex flex-col">
+      <Navbar />
+      <Hero />
+      <Intro />
+      <AwardsAndRecognitions />
+      <Features />
+      <Updates />
+      <Compliance />
+    </div>
+  );
 };
 
 export default Page;
