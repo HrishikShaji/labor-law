31fc877b83cf51d5b8694c97119327fd250cd61a
HrishikShaji <hrishikminishaji@gmail.com>
2024-04-28 22:33:10 +0530

    added features

--- app/landing/page.tsx
+++ app/landing/page.tsx
@@ -1,17 +1,19 @@
 import { AwardsAndRecognitions } from "./components/AwardsAndRecognitions";
+import { Features } from "./components/Features";
 import { Hero } from "./components/Hero";
 import { Intro } from "./components/Intro";
 import { Navbar } from "./components/Navbar";
 
 const Page = () => {
-	return (
-		<div className="w-full  h-full flex flex-col">
-			<Navbar />
-			<Hero />
-			<Intro />
-			<AwardsAndRecognitions />
-		</div>
-	);
+  return (
+    <div className="w-full  h-full flex flex-col">
+      <Navbar />
+      <Hero />
+      <Intro />
+      <AwardsAndRecognitions />
+      <Features />
+    </div>
+  );
 };
 
 export default Page;
