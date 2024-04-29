4deb1192c6e72e1875c618dc2e24e67dfc8cd1f6
HrishikShaji <hrishikminishaji@gmail.com>
2024-04-29 06:26:03 +0530

    footer done

--- app/landing/page.tsx
+++ app/landing/page.tsx
@@ -1,23 +1,26 @@
 import { AwardsAndRecognitions } from "./components/AwardsAndRecognitions";
 import { Compliance } from "./components/Compliance";
 import { Features } from "./components/Features";
+import { Footer } from "./components/Footer";
 import { Hero } from "./components/Hero";
 import { Intro } from "./components/Intro";
 import { Navbar } from "./components/Navbar";
 import { Updates } from "./components/Updates";
 
 const Page = () => {
-  return (
-    <div className="w-full  h-full flex flex-col">
-      <Navbar />
-      <Hero />
-      <Intro />
-      <AwardsAndRecognitions />
-      <Features />
-      <Updates />
-      <Compliance />
-    </div>
-  );
+	return (
+		<div className="w-full  h-full flex flex-col">
+			<Navbar />
+			<Hero />
+			<Intro />
+			<AwardsAndRecognitions />
+			<Features />
+			<Updates />
+			<Compliance />
+
+			<Footer />
+		</div>
+	);
 };
 
 export default Page;
